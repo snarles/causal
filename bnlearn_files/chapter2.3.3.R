a5f299d295fb208449ad6a906e28f211c6b4d9a8
snarles <snarles@stanford.edu>
2016-02-11 11:34:14 -0800

    typed script

---
+++ bnlearn_files/chapter2.3.3.R
@@ -0,0 +1,6 @@
+hl2 = list(arcs = vstructs(dag2, arcs = TRUE), lwd = 4, col = "black")
+hl3 = list(arcs = vstructs(dag3, arcs = TRUE), lwd = 3, col = "black")
+graphviz.plot(dag2, highlight = hl2, layout = "fdp", main = "dag2")
+graphviz.plot(dag3, highlight = hl3, layout = "fdp", main = "dag3")
+graphviz.plot(cpdag(dag2), highlight = hl2, layout = "fdp", main = "dag2")
+graphviz.plot(cpdag(dag3), highlight = hl3, layout = "fdp", main = "dag3")
